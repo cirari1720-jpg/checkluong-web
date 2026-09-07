ebb3e1b2c3f77594258eb7e4d66328bf62f2341a
AdminSH <cirari1720@gmail.com>
2026-09-07 21:45:05 +0700

    add website open graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://checkluong-web-4z0u.vercel.app"),
   title: "Tra cứu thông tin",
   description: "Quản lý KPI và thông tin Staff",
 };
